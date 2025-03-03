7ecb3f40eebe90c6be839ee51ae42b3b8aa5b0cf
lucasramallo <lucasdebrito.jd@gmail.com>
2025-03-03 15:18:10 -0300

    testes com if

--- TaxInvoiceProcessing/scripts/testes.sql
+++ TaxInvoiceProcessing/scripts/testes.sql
@@ -3,6 +3,23 @@ SELECT * FROM TB_NOTA_FISCAL WHERE STATUS = 'Pendente';
 
 DESCRIBE TB_NOTA_FISCAL;
 
-
-
 ALTER SESSION SET CURRENT_SCHEMA = TAXINVOICE;
+
+
+SET SERVEROUTPUT ON
+DECLARE
+    vNf_status TB_NOTA_FISCAL.STATUS%TYPE;
+BEGIN    
+    SELECT status 
+    INTO   vNf_status
+    FROM  TB_NOTA_FISCAL
+    WHERE numero = '000012345';
+    
+    IF vNf_status = 'Pendente' THEN
+        dbms_output.put_line('Essa nota fiscal está Pendente');
+    ELSE
+        dbms_output.put_line('Essa nota fiscal não está Pendente');
+    END IF;
+    
+    dbms_output.put_line(vNf_status);
+END;
